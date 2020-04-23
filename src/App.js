6509ceb3ae6bef02276bddc42c41b92b994778b9
Paula Goicoechea <paula.goicoechea@gmail.com>
2020-04-23 12:38:11 +0200

    Update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
-
+import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+  return(
+    <div>
+        HATS
+    </div>
+  );
+}
+
 
 function App() {
   return <div>
-    <HomePage/>
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/shop/hats' component={HatsPage}/>
+    </Switch>
   </div>
   
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'; //higher order component: a function that takes a component as an agument and
+//returns a modified component, in this case we are going to modify MenuItem to have access to the things related to the router
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => ( //aca podríamos pasar props y luego acceder como props.title
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => ( //aca podríamos pasar props y luego acceder como props.title
+    <div className={`${size} menu-item`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className = 'background-image' style={{
             backgroundImage: `url(${imageUrl})`
         }}
@@ -17,4 +20,4 @@ const MenuItem = ({title, imageUrl, size}) => ( //aca podríamos pasar props y l
 
 );
 
-export default MenuItem;
+export default withRouter(MenuItem); //returns a menuitem component with access to location match and history of the router!!!!
